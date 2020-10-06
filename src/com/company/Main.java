408c0bb6c33102c58e0f9cb18a205f51acc65c3e
Plumb98 <plumbic98@mail.ru>
2020-10-06 14:32:16 +0300

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	// add any comment for second commit
         System.out.println("Hello World");
     }
 }
