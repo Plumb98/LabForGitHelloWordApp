9941e51418b8cf679bf357dffacbd794c5de15ab
Plumb98 <plumbic98@mail.ru>
2020-10-06 15:04:31 +0300

    some changes were added to the main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+	// some changes were added to the main class
         System.out.println("Hello World");
     }
 }
